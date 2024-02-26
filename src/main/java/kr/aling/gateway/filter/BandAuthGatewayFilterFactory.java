c5414ca6f21a7731286694ac92cb19bb86c654d5
hobby3530 <jungy3530@gmail.com>
2024-02-26 23:07:23 +0900

    [Refactor] FeignClient 사용
    - webclient -> feignClient 변경

--- src/main/java/kr/aling/gateway/filter/BandAuthGatewayFilterFactory.java
+++ src/main/java/kr/aling/gateway/filter/BandAuthGatewayFilterFactory.java
@@ -1,13 +1,12 @@
 package kr.aling.gateway.filter;
 
 import java.util.Objects;
-import kr.aling.gateway.common.dto.GetBandUserAuthResponseDto;
-import kr.aling.gateway.common.properties.AlingUrlProperties;
+import kr.aling.gateway.feignclient.UserServerClient;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * 그룹 회원 권한 필터.
@@ -21,11 +20,11 @@ public class BandAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<B
     private static final String X_TEMP_USER_NO = "X-TEMP-USER-NO";
     private static final String X_BAND_USER_ROLE = "X-BAND-USER-ROLE";
 
-    private final AlingUrlProperties alingUrlProperties;
+    private final UserServerClient userServerClient;
 
-    public BandAuthGatewayFilterFactory(AlingUrlProperties alingUrlProperties) {
+    public BandAuthGatewayFilterFactory(@Lazy UserServerClient userServerClient) {
         super(Config.class);
-        this.alingUrlProperties = alingUrlProperties;
+        this.userServerClient = userServerClient;
     }
 
     @Override
@@ -40,11 +39,9 @@ public class BandAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<B
             Long userNo = Long.parseLong(Objects.requireNonNull(request.getHeaders().get(X_TEMP_USER_NO)).get(0));
             Long bandNo = Long.parseLong(Objects.requireNonNull(request.getHeaders().get(X_BAND_NO)).get(0));
 
-            GetBandUserAuthResponseDto dto = WebClient.create(
-                            alingUrlProperties.getUserUrl() + "/api/v1/bands/" + bandNo + "/users/" + userNo + "/role")
-                    .get().retrieve().bodyToMono(GetBandUserAuthResponseDto.class).block();
-
-            exchange.getRequest().mutate().header(X_BAND_USER_ROLE, dto.getBandUserRoleName()).build();
+            exchange.getRequest().mutate()
+                    .header(X_BAND_USER_ROLE, userServerClient.getUserById(bandNo, userNo).getBandUserRoleName())
+                    .build();
 
             return chain.filter(exchange);
         });
