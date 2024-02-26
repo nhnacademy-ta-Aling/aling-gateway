a84c7afde1a9776e9c373e66d8cfb3807e3ec9c4
hobby3530 <jungy3530@gmail.com>
2024-02-26 23:07:41 +0900

    [Refactor] FeignClient 사용
    - webclient -> feignClient 변경

--- src/main/java/kr/aling/gateway/feignclient/UserServerClient.java
+++ src/main/java/kr/aling/gateway/feignclient/UserServerClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @author 정유진
  * @since 1.0
  **/
-@FeignClient(name="aling-user", configuration = OpenFeignConfig.class)
+@FeignClient(name = "aling-user", configuration = OpenFeignConfig.class)
 public interface UserServerClient {
     @GetMapping("/api/v1/bands/{bandNo}/users/{userNo}/role")
     GetBandUserAuthResponseDto getUserById(@PathVariable("bandNo") Long bandNo, @PathVariable("userNo") Long userNo);
